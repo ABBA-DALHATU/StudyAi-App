f5b6dd4c83589ff2b221afca5aa21d102616a6d2
Abba Dalhatu <abbadalhat101@gmail.com>
2025-05-06 09:55:46 +0100

    i did something diabolical so that i can host faster

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,14 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+    tsconfigPath: "tsconfig.json",
   },
   eslint: {
     ignoreDuringBuilds: true,
+    dirs: ["pages", "components", "lib", "utils", "hooks", "src"],
   },
+  swcMinify: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
